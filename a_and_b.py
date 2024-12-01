2ee4422f6a0c3a15d12ffc44049b1650393d48cb
Sepelire <arthurpan2001@mail.ru>
2024-12-02 01:04:28 +0500

    Complete: task a_and_b

---
+++ a_and_b.py
@@ -0,0 +1,10 @@
+# Ввод: 2 целых числа, разделённые пробелом
+# Вывод: сумма чисел
+string = input()
+array_str = string.split()
+a = int(array_str[0])
+b = int(array_str[1])
+print(a + b)
+# -3 5
+
+# 2
