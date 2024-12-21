cd85f629e6d29d72047ef92ab4bffb1cb32ad33b
Sepelire <arthurpan2001@mail.ru>
2024-12-21 19:38:19 +0500

    Task 15

---
+++ sgd_3.py
@@ -0,0 +1,15 @@
+numbers_str = input().split()
+numbers_arr = []
+for num_str in numbers_str:
+    numbers_arr.append(int(num_str))
+print(numbers_arr)
+
+while len(numbers_arr) > 1:
+    a = numbers_arr[0]
+    b = numbers_arr[1]
+
+    while b:
+        a, b = b, a % b
+    numbers_arr.pop(1)
+    numbers_arr[0] = a
+print(numbers_arr[0])
